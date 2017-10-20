5c460b976946718979ba0b97038b0aee1f7d0111
Etienne FONTAINE <etienne.fontaine1@gmail.com>
2017-10-20 12:15:04 +0200

    update functions

--- wp-content/themes/hetic/functions.php
+++ wp-content/themes/hetic/functions.php
@@ -6,18 +6,77 @@
  * Time: 14:08
  */
 
-define( 'THEME_PATH' ,          get_template_directory()            );
+define( 'THEME_PATH', get_template_directory()            );
 //define( 'TEMPLATE_PATH' ,       THEME_PATH .    '/templates'        );
 
-define( 'THEME_URL' ,           get_template_directory_uri()        );
-define( 'CSS_URL' ,             THEME_URL .    '/assets/css'       );
-define( 'IMAGES_URL' ,          THEME_URL .    '/assets/img'       );
-define( 'VIDEOS_URL' ,          THEME_URL .    '/assets/video'       );
-define( 'JS_URL' ,              THEME_URL .    '/assets/js'      );
+define( 'THEME_URL', get_template_directory_uri()        );
+define( 'CSS_URL', THEME_URL .    '/assets/css'       );
+define( 'IMAGES_URL', THEME_URL .    '/assets/img'       );
+define( 'VIDEOS_URL', THEME_URL .    '/assets/video'       );
+define( 'JS_URL', THEME_URL .    '/assets/js'      );
 //define( 'FAVICONS_URL' ,        THEME_URL .    '/dist/favicon'      );
 //define( 'ADMIN_IMAGES_URL' ,    IMAGES_URL .   '/admin'             );
 
 
-foreach ( glob( THEME_PATH . "/inc/*.php" ) as $file ) {
-	include_once $file;
+foreach (glob( THEME_PATH . "/inc/*.php" ) as $file) {
+    include_once $file;
 }
+
+
+// SECURITY
+function wpb_remove_version()
+{
+    return '';
+}
+add_filter('the_generator', 'wpb_remove_version');
+
+function remove_footer_admin()
+{
+    echo '<p>Made by P2020 at HETIC</p>';
+}
+add_filter('admin_footer_text', 'remove_footer_admin');
+
+function no_wordpress_errors()
+{
+    return 'Something is wrong!';
+}
+add_filter( 'login_errors', 'no_wordpress_errors' );
+
+remove_filter( 'authenticate', 'wp_authenticate_email_password', 20 );
+
+function fb_filter_query($query, $error = true)
+{
+    if (is_search()) {
+        $query->is_search = false;
+        $query->query_vars[s] = false;
+        $query->query[s] = false;
+ 
+        // to error
+        if ($error == true) {
+            $query->is_404 = true;
+        }
+    }
+}
+
+function my_myme_types($mime_types)
+{
+    $mime_types['svg'] = 'image/svg+xml'; //Adding svg extension
+    return $mime_types;
+}
+add_filter('upload_mimes', 'my_myme_types', 1, 1);
+
+function wpb_imagelink_setup()
+{
+    $image_set = get_option( 'image_default_link_type' );
+     
+    if ($image_set !== 'none') {
+        update_option('image_default_link_type', 'none');
+    }
+}
+add_action('admin_init', 'wpb_imagelink_setup', 10);
+
+add_filter('xmlrpc_enabled', '__return_false');
+ 
+add_action( 'parse_query', 'fb_filter_query' );
+add_filter( 'get_search_form', create_function( '$a', "return null;" ) );
+remove_action('welcome_panel', 'wp_welcome_panel');
--- wp-content/themes/hetic/template-home.php
+++ wp-content/themes/hetic/template-home.php
@@ -11,113 +11,113 @@
             <div class="section-social-content-about">
                 <div class="section-social-content-about--img" style="background-image:url('<?php echo IMAGES_URL ?>/JR-profile.jpg')" ></div>
                 <span class="section-social-content-about--name">
-              <?php the_field('section_1_user_name') ?>
+                <?php the_field('section_1_user_name') ?>
             </span>
             </div>
             <div class="section-social-content--text">
                 <p>
-                    <?php 
+                    <?php
                     $str = file_get_contents(THEME_URL . '/instagram/instagram.json');
                     $json = json_decode($str);
                     echo $json->desc
-                    // the_field('section_1_text') 
+                    // the_field('section_1_text')
                     ?>
                 </p>
             </div>
             <div class="section-social-content--date">
-                <?php echo $json->date ?>
+                <time datetime="<?php echo $json->photoTime ?>">LA DATE FORMATÉE !! </time>
             </div>
         </div>
         <div class="section-social-links">
             <a href="<?php the_field('section_1_instagram_link') ?>" target="_blank" class="section-social-links-link section-social-links-link--insta">
-	            <?php the_field('section_1_instagram_text') ?>
+                <?php the_field('section_1_instagram_text') ?>
             </a>
             <a href="<?php the_field('section_1_twitter_link') ?>" target="_blank" class="section-social-links-link section-social-links-link--twitter">
-	            <?php the_field('section_1_twitter_text') ?>
+                <?php the_field('section_1_twitter_text') ?>
             </a>
         </div>
     </section>
-	<?php
-	$lastposts = get_posts( array(
-		'posts_per_page' => 4,
-		'post_type' => 'news',
-		'orderby' => 'date',
-		'order' => 'DESC'
-	) );
+    <?php
+    $lastposts = get_posts( array(
+        'posts_per_page' => 4,
+        'post_type' => 'news',
+        'orderby' => 'date',
+        'order' => 'DESC'
+    ) );
 
-	if ( $lastposts ) { ?>
+    if ($lastposts) { ?>
         <section class="section section-news">
             <h2 class="section-news--title section--title">
                 news
             </h2>
             <div class="section-news-wrapper">
-				<?php
-				foreach ( $lastposts as $post ) :
-					setup_postdata( $post ); ?>
+                <?php
+                foreach ($lastposts as $post) :
+                    setup_postdata( $post ); ?>
                     <a class="section-news-wrapper-item" href="<?php the_permalink() ?>">
-	                    <?php $image = get_field( 'photo' ); ?>
+                        <?php $image = get_field( 'photo' ); ?>
                         <div class="section-news-wrapper-item--img"
                              style="background-image:url('<?php echo $image['url']; ?>')"></div>
                         <div class="section-news-wrapper-item--date">
                             <?php the_date() ?>
                         </div>
                         <div class="section-news-wrapper-item--title">
-							<?php the_title(); ?>
+                            <?php the_title(); ?>
                         </div>
                         <div class="section-news-wrapper-item--text">
-							<?php the_content(); ?>
+                            <?php the_content(); ?>
                         </div>
                     </a>
-					<?php
-				endforeach;
-				wp_reset_postdata();
-				?>
+                    <?php
+                endforeach;
+                wp_reset_postdata();
+                ?>
             </div>
             <a href="<?php echo get_page_link( get_page_by_title( 'news' )->ID ); ?>" class="section-news--link section--link">
                 Read more articles
             </a>
         </section>
-		<?php
-	}
-	$lastposts = get_posts( array(
-		'posts_per_page' => 4,
-		'post_type' => 'projects',
-		'orderby' => 'date',
-		'order' => 'DESC'
-	) );
+        <?php
+    }
+    $lastposts = get_posts( array(
+        'posts_per_page' => 4,
+        'post_type' => 'projects',
+        'orderby' => 'date',
+        'order' => 'DESC'
+    ) );
 
-	if ( $lastposts ) { ?>
+    if ($lastposts) { ?>
     <section class="section section-projects">
         <h2 class="section-projects--title section--title">
             Latest Projects
         </h2>
         <div class="section-projects-wrapper">
             <?php
-		        foreach ( $lastposts as $post ) :
-			        setup_postdata( $post ); ?>
+            foreach ($lastposts as $post) :
+                setup_postdata( $post ); ?>
                     <a class="section-projects-wrapper-item" href="<?php the_permalink() ?>">
                         <div class="section-projects-wrapper-item--date">
-	                        <?php the_field('year'); ?>
+                            <?php the_field('year'); ?>
                         </div>
-	                    <?php $image = get_field( 'photo' ); ?>
+                        <?php $image = get_field( 'photo' ); ?>
                         <div class="section-projects-wrapper-item--img" style="background-image:url('<?php echo $image['url']; ?>')"></div>
                         <div class="section-projects-wrapper-item--title">
-	                        <?php the_field('title'); ?>
+                            <?php the_field('title'); ?>
                         </div>
                         <div class="section-projects-wrapper-item--tags">
-	                        <?php the_field('tags'); ?>
+                            <?php the_field('tags'); ?>
                         </div>
                     </a>
-			        <?php
-		        endforeach;
-		        wp_reset_postdata();
-	        ?>
+                    <?php
+            endforeach;
+                wp_reset_postdata();
+            ?>
         </div>
         <a href="<?php echo get_page_link( get_page_by_title( 'projects' )->ID ); ?>" class="section-projects--link section--link">
             Discover more projects
         </a>
     </section>
-    <?php } ?>
+    <?php                                                                                                         } ?>
     <section class="section section-exhibitions">
     <div class="section-exhibitions--title section--title">
       exhibitions
@@ -356,18 +356,18 @@
                 <br/>
                 <br/>
                 However, it was not until he found a camera on the subway that his perception on street art change. This allowed him to track the individuals who communicate messages via walls and street art. He quickly began to track people in the forbidden undergrounds and roofs of Paris, France. In 2004, street artist JR photographed the riots that broke out in the banlieues and created his first major project by pasting up large prints of their faces around the city.
-                <a href="#" class="section-about-wrapper--link">
+                <a href="<?php echo get_page_link( get_page_by_title( 'jr' )->ID ); ?>" class="section-about-wrapper--link">
                     Read more
                 </a>
             </div>
-            <div class="section-about-wrapper-img" style="background-image:url(<?php echo IMAGES_URL ?>'/JR-about.jpg')"></div>
+            <div class="section-about-wrapper-img" style="background-image:url('<?php echo IMAGES_URL ?>/JR-about.jpg')"></div>
         </div>
         <div class="section-about-social">
             <a href="<?php the_field('section_1_instagram_link') ?>" target="_blank" class="section-about-social--link">
-		        <?php the_field('section_1_instagram_text') ?>
+                <?php the_field('section_1_instagram_text') ?>
             </a>
             <a href="<?php the_field('section_1_twitter_link') ?>" target="_blank" class="section-about-social--link">
-		        <?php the_field('section_1_twitter_text') ?>
+                <?php the_field('section_1_twitter_text') ?>
             </a>
         </div>
     </section>
