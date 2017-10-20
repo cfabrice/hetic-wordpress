ef1fa74bc7cf0badb85f538dc7a55ef1307b038c
Etienne FONTAINE <etienne.fontaine1@gmail.com>
2017-10-20 10:46:46 +0200

    update nodejs

--- wp-content/themes/hetic/instagram/index.js
+++ wp-content/themes/hetic/instagram/index.js
@@ -1,31 +1,66 @@
 var fs = require('fs'),
- Nightmare = require('nightmare');
-const { exec } = require('child_process');
+    Nightmare = require('nightmare');
+const {
+    exec
+} = require('child_process');
 
 var url = 'https://www.instagram.com/jr/?hl=fr';
-var nightmare = Nightmare({ show: false });
+var nightmare = Nightmare({
+    show: false
+});
+var nightmare2 = Nightmare({
+    show: true
+});
+
+
+function getImage() {
+    nightmare.goto(url)
+        .wait()
+        .evaluate(function () {
+            const img = document.querySelector('body > span > section > main > article > div > div > div > div > a > div > div > img')
+            const link = document.querySelector('body > span > section > main > article > div > div > div > div > a')
+            const isVideo = document.querySelector('body > span > section > main > article > div > div > div > div > a').childElementCount > 1 ? true : false;
+            let src = img.src.split('/').pop()
+
+            return {
+                src,
+                desc: img.alt,
+                link: link.href,
+                isVideo
+            };
+        })
+        .end()
+        .then(function (file) {
+            if (file) {
+                getDate(file)
+            }
+        });
+}
+
+function getDate(file) {
+    console.log('getting date...');
+    console.log(file)
+    nightmare2.goto(file.link)
+        .wait()
+        .evaluate(function () {
+            let photoTime = document.querySelector('time').getAttribute('datetime')
+            return photoTime
+        })
+        .end()
+        .then(function (photoTime) {
+            file.photoTime = photoTime
+            const json = JSON.stringify(file)
+            fs.writeFile('instagram.json', json, 'utf8')
+            exec('wget https://scontent-cdt1-1.cdninstagram.com/t51.2885-15/e35/' + file.src + ' -O photo.jpg', (err, stdout, stderr) => {
+                if (err) {
+                    // node couldn't execute the command
+                    return;
+                }
+            });
+        });
+}
+
 var scrapper = nightmare.goto(url)
-                        .wait()
-						.evaluate(function(){
-                            const img = document.querySelector('body > span > section > main > article > div > div > div > div > a > div > div > img')
-                            const link = document.querySelector('body > span > section > main > article > div > div > div > div > a')
-                            let src = img.src.split('/').pop()
-
-
-                            return {
-                                src,
-                                desc : img.alt,
-                                link : link.href
-                            };
-                        })
-                        .end()
-                        .then(function(img){
-                            const json = JSON.stringify(img)
-                            fs.writeFile('instagram.json', json, 'utf8')
-                            exec('wget https://scontent-cdt1-1.cdninstagram.com/t51.2885-15/e35/' + img.src + ' -O photo.jpg', (err, stdout, stderr) => {
-                              if (err) {
-                                // node couldn't execute the command
-                                return;
-                              }
-                            });
-                        });
+    .run(function (err, nightmare) {
+        getImage();
+    });
