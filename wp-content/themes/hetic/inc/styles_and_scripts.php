54af2a66976bc2a23514ea414018747187018045
Etienne FONTAINE <etienne.fontaine1@gmail.com>
2017-11-03 14:09:03 +0100

    update videos

--- wp-content/themes/hetic/inc/styles_and_scripts.php
+++ wp-content/themes/hetic/inc/styles_and_scripts.php
@@ -10,39 +10,59 @@
 add_action( 'wp_enqueue_scripts', 'ajout_scripts' );
 add_action( 'after_setup_theme', 'thumbnails_theme_support' );
 
-function ajout_scripts() {
+function ajout_scripts()
+{
 
 
-	wp_register_script('main_script', JS_URL . '/main.min.js', array('jquery'),'1.1', true);
-	wp_enqueue_script('main_script');
-
-
-
-	wp_register_style( 'main_style', CSS_URL . '/app.min.css' );
-	wp_enqueue_style( 'main_style' );
+    wp_register_script('main_script', JS_URL . '/main.min.js', array('jquery'), '1.1', true);
+    wp_enqueue_script('main_script');
 
 
 
-	wp_register_style( 'google_font', 'https://fonts.googleapis.com/css?family=Alegreya+Sans|Alegreya+Sans+SC,300,400,500|Cabin,400,500,600,700' );
-	wp_enqueue_style( 'google_font' );
-
-}
-function thumbnails_theme_support(){
-	add_theme_support( 'post-thumbnails' );
+    wp_register_style( 'main_style', CSS_URL . '/app.min.css' );
+    wp_enqueue_style( 'main_style' );
 }
 
+function wpb_add_google_fonts()
+{
+ 
+    wp_enqueue_style( 'wpb-google-fonts', 'https://fonts.googleapis.com/css?family=Alegreya+Sans|Alegreya+Sans+SC|Cabin', false );
+}
+ 
+add_action( 'wp_enqueue_scripts', 'wpb_add_google_fonts' );
 
-function jr_menus() {
-	register_nav_menus( array(
-		'header-menu-right' => 'Header menu right',
-		'header-menu-left' => 'Header menu left',
-	) );
+function thumbnails_theme_support()
+{
+    add_theme_support( 'post-thumbnails' );
 }
 
-function wpc_mime_types($mimes) {
-	$mimes['svg'] = 'image/svg+xml';
-	return $mimes;
+
+function jr_menus()
+{
+    register_nav_menus( array(
+        'header-menu-right' => 'Header menu right',
+        'header-menu-left' => 'Header menu left',
+    ) );
 }
+
+function wpc_mime_types($mimes)
+{
+    $mimes['svg'] = 'image/svg+xml';
+    return $mimes;
+}
+
+add_filter('wp_handle_upload_prefilter', 'custom_upload_filter' );
+
+function process_images($results)
+{
+    if ($results['type'] === 'video/*') {
+        $cmd = "ffmpeg -ss 4 -i ".$results." -s 1920x1080 -frames:v 1 ".$results['name'].".jpg";
+        exec($cmd, $thumb_stdout, $retval);
+    }
+}
+
+add_action('wp_handle_upload', 'process_images');
+
 add_filter('upload_mimes', 'wpc_mime_types');
 add_action( 'init', 'jr_menus' );
 
